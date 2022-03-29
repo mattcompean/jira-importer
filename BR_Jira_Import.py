3faecd1bc0a9575fdef30a9bf4caa238e69071fa
Matthew Compean <matthew.compean@pixelogicmedia.com>
2022-03-28 22:27:06 -0700

    updated BugReport class with correct fields, added stubs for jira create issue and parseReport

--- BR_Jira_Import.py
+++ BR_Jira_Import.py
@@ -3,17 +3,36 @@ from genericpath import isdir
 import sys
 import tarfile
 import os
-from tempfile import tempdir
+import json
 from jira import JIRA
 
 APPLICATION_TOKEN = ''
+JIRA_HOSTNAME = 'https://jira.atlassian.com'
 TEMP_DIRECTORY = 'H:/Work/Python/temp'
 
 #TODO add all necessary fields from bug report file
 class BugReport:
     def __init__(self):
+        self.projectName = "SAGE"
         self.issueNumber = ""
+        self.summary = ""
+        self.project = ""
+        self.assignee = ""
+        self.originator = ""
+        self.affectedVersion = ""
+        self.stringName = ""
+        self.fundingsource = ""
+        self.model = ""
         self.component = ""
+        self.type = ""
+        self.description = ""
+        self.priority = ""
+        self.date = ""
+        self.category = ""
+        self.tsdLocation = ""
+        self.logLocation = ""
+        self.commLocation = ""
+        self.attachments = ["", ""]
 
 def extractTar(tarPath):
     print("Extracting " + tarPath + " to " + TEMP_DIRECTORY)
@@ -32,8 +51,18 @@ def getReportPaths():
 
     return reportPaths
 
+def parseReport(reportPath):
+    fstream = open(reportPath, "r")
+    data = json.loads(fstream.read())
+    fstream.close()
+    bugReport = BugReport()
+
+    bugReport.description = data["description"]
+
+    return bugReport
+
 def importToJira():
-    jiraClient = JIRA('https://jira.atlassian.com', token_auth=APPLICATION_TOKEN)
+    jiraClient = JIRA(server=JIRA_HOSTNAME, token_auth=APPLICATION_TOKEN)
     reportPaths = getReportPaths()
 
     if len(reportPaths) > 0:
@@ -41,6 +70,12 @@ def importToJira():
     else:
         print("Error: Could not get list of bug reports")
         exit(1)
+
+    for report in reportPaths:
+        for file in os.scandir(report):
+            if file.path.endswith(".json"):
+                br:BugReport = parseReport(file.path)
+                jiraClient.create_issue(project=br.project, summary=br.summary, description=br.description)
 
 
 def main(argv):
