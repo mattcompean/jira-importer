81729e98ad3d1981cb0abd7aa1abc7b590461277
Matthew Compean <matthew.compean@pixelogicmedia.com>
2022-05-17 18:17:31 -0700

    removed extraneous "and" condition from tarfile validation, changed report to reportPath for easier reading, added try/except block for authenticating the Jira Client

--- BR_Jira_Import.py
+++ BR_Jira_Import.py
@@ -6,10 +6,11 @@ import os
 import json
 from jira import JIRA
 from jira import Issue
+from jira import JIRAError
 
 APPLICATION_TOKEN = ''
 JIRA_HOSTNAME = 'https://jira.atlassian.com'
-TEMP_DIRECTORY = 'H:/Work/Python/temp'
+OUTPUT_DIRECTORY = 'H:/Work/Python/temp'
 
 class BugReport:
     def __init__(self):
@@ -35,17 +36,17 @@ class BugReport:
         self.attachments = ["", ""]
 
 def extractTar(tarPath):
-    print("Extracting " + tarPath + " to " + TEMP_DIRECTORY)
+    print("Extracting " + tarPath + " to " + OUTPUT_DIRECTORY)
     tar = tarfile.open(tarPath)
-    tar.extractall(TEMP_DIRECTORY)
+    tar.extractall(OUTPUT_DIRECTORY)
     tar.close()
 
 def getReportPaths():
     print("Getting list of bug reports")
 
     reportPaths:array
-    for dir in os.scandir(TEMP_DIRECTORY):
-        fullPath = os.path.join(TEMP_DIRECTORY, dir)
+    for dir in os.scandir(OUTPUT_DIRECTORY):
+        fullPath = os.path.join(OUTPUT_DIRECTORY, dir)
         if isdir(fullPath):
             reportPaths.append(fullPath)
 
@@ -79,13 +80,19 @@ def parseReport(reportPath):
 
     return bugReport
 
-def attachScreenShots(jiraClient:JIRA, report, issue:Issue):
-    for file in os.scandir(report):
+def attachScreenShots(jiraClient:JIRA, reportPath, issue:Issue):
+    print("Adding screen shots as attachments to issue " + issue.id)
+    for file in os.scandir(reportPath):
         if file.path.endswith(".png") or file.path.endswith(".jpg"):
             jiraClient.add_attachment(issue, attachment=file.path)
 
 def importToJira():
-    jiraClient = JIRA(server=JIRA_HOSTNAME, token_auth=APPLICATION_TOKEN)
+    try:
+        jiraClient = JIRA(server=JIRA_HOSTNAME, token_auth=APPLICATION_TOKEN)
+    except JIRAError as err:
+        print("Error: An exception occured while attempting to authenticate the Jira Client: " + err.text)
+        exit(1)
+
     reportPaths = getReportPaths()
 
     if len(reportPaths) > 0:
@@ -94,14 +101,19 @@ def importToJira():
         print("Error: Could not get list of bug reports")
         exit(1)
 
-    for report in reportPaths:
-        for file in os.scandir(report):
+    for reportPath in reportPaths:
+        for file in os.scandir(reportPath):
             if file.path.endswith(".json"):
                 br:BugReport = parseReport(file.path)
                 issue = jiraClient.create_issue(project=br.project, summary=br.summary, description=br.description)
-                comment = "Log Locations: " + TEMP_DIRECTORY + "/" + br.tsdLocation + ", " + TEMP_DIRECTORY + "/" + br.logLocation + ", " + TEMP_DIRECTORY + "/" + br.commLocation
+                print("Created issue: " + issue.id)
+                comment = "Log Locations: " + reportPath + "/" + br.tsdLocation + ", " + reportPath + "/" + br.logLocation + ", " + reportPath + "/" + br.commLocation
                 jiraClient.add_comment(issue, comment)
-                attachScreenShots(jiraClient, report, issue)
+                attachScreenShots(jiraClient, reportPath, issue)
+
+                #Possible improvements later: add 
+
+    jiraClient.close()
 
 
 def main(argv):
@@ -115,7 +127,7 @@ def main(argv):
         print("Usage: BR_Jira_Import.py <Path to Bug Report Tar>")
         sys.exit(1)
 
-    if brTar.endswith('.tar', '.tar.gz', '.tgz', '.gz') and os.path.exists(brTar):
+    if brTar.endswith('.tar', '.tar.gz', '.tgz', '.gz'):
         if os.path.exists(brTar):
             extractTar(brTar)
         else:
